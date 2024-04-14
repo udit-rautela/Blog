ed0a2d68a9027149c7a438f8d01d44c3561f0ea1
Udit Rautela <109891484+udit-rautela@users.noreply.github.com>
2024-04-14 15:37:48 +0530

    Add files via upload

--- admin/partials/header.php
+++ admin/partials/header.php
@@ -1,1 +1,9 @@
+<?php
+    require '../partials/header.php';
+
+     // check login status   or ACCESS control 
+     if(!isset($_SESSION['user-id'])){
+        header('location:' . ROOT_URL . 'signin.php');
+        die();
+     } 
 
